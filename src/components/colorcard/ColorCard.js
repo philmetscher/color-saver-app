5c16a69f4c6e5cb3754a82c670a55138114d2b25
Phil Metscher <philmetschermailt@gmx.de>
2022-09-23 16:23:02 +0200

    add clipboard function

--- src/components/colorcard/ColorCard.js
+++ src/components/colorcard/ColorCard.js
@@ -1,12 +1,17 @@
 import "./ColorCard.css";
 
 export default function ColorCard({ card }) {
+  const handleCardClick = () => {
+    navigator.clipboard.writeText(card.hexCode);
+  };
+
   return (
     <li
       className="card color-card"
       style={{
         backgroundColor: card.hexCode,
       }}
+      onClick={handleCardClick}
     >
       <input
         className="card-code card-code__hex"
